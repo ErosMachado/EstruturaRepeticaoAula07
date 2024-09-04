d37df229633243f48089386ef73ebd3a6ba5a789
ErosMachado <erosrodriguesmachado@gmail.com>
2024-09-04 14:54:52 -0300

    atualização de nome do arquivo

--- EstruturaRepeticao/src/ResolucaoExercicios/Exercicio4.java
+++ EstruturaRepeticao/src/ResolucaoExercicios/Exercicio4.java
@@ -1,6 +1,6 @@
 package ResolucaoExercicios;
 import java.util.Scanner;
-public class Exercicios4 {
+public class Exercicio4 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in); 
