6406bcd178a91ddb6d70f9ded461e220309c6b1f
ErosMachado <erosrodriguesmachado@gmail.com>
2024-09-04 17:26:40 -0300

    Exercicio 9 feito

--- EstruturaRepeticao/src/ResolucaoExercicios/Exercicio9.java
+++ EstruturaRepeticao/src/ResolucaoExercicios/Exercicio9.java
@@ -5,7 +5,17 @@ public class Exercicio9 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-
+		int memoria=0;
+		
+		System.out.println("Digite um número inteiro positivo: ");
+		int num = scan.nextInt();
+		
+		for(int i=1; i<=num;i++) {
+			if(num%i==0) {
+				memoria = i;
+				System.out.println(memoria);
+			}
+		}
 		scan.close();
 	}
 
