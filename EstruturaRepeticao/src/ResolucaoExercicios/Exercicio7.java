96da6b14ebf2a1fce5a028662571eae2589c1c25
ErosMachado <erosrodriguesmachado@gmail.com>
2024-09-04 16:05:23 -0300

    resolução 7

--- EstruturaRepeticao/src/ResolucaoExercicios/Exercicio7.java
+++ EstruturaRepeticao/src/ResolucaoExercicios/Exercicio7.java
@@ -5,11 +5,26 @@ import java.util.Scanner;
 public class Exercicio7 {
 
 	public static void main(String[] args) {
+		double notaAluno;
+		int numSuperior=0, numInferior=0;
 		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("Informe a média da sala: ");
+		double mediaSala = scan.nextDouble();
 		
-		
+		for(int i=1; i<=20; i++) {
+			System.out.println("Informe a média do aluno número "+i);
+			notaAluno = scan.nextDouble();
+			if(notaAluno>mediaSala) {
+				numSuperior= numSuperior+1;
+			}else if(notaAluno<mediaSala) {
+				numInferior = numInferior+1;
+			}else {
+				
+			}
+		}
+		System.out.println("O número de alunos que tiveram nota acima da média da sala foi "+numSuperior);
+		System.out.println("O número de alunos que tiveram nota abaixo da média da sala foi "+numInferior);
 		scan.close();
 	}
-
 }
