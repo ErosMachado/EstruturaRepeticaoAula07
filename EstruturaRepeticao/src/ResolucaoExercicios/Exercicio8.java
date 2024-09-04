dfb5807024027ad9d0cc37154f9c7c266bd576e1
ErosMachado <erosrodriguesmachado@gmail.com>
2024-09-04 17:15:59 -0300

    Exercicio 8 e 9

---
+++ EstruturaRepeticao/src/ResolucaoExercicios/Exercicio8.java
@@ -0,0 +1,27 @@
+package ResolucaoExercicios;
+import java.util.Scanner;
+
+public class Exercicio8 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num;
+
+        do {
+            System.out.println("Digite um número inteiro positivo: ");
+            while (!scan.hasNextInt()) { 
+                System.out.println("Entrada inválida! Digite um número inteiro positivo: ");
+                scan.next(); 
+            }
+            num = scan.nextInt();
+            if (num <= 0) {
+                System.out.println("O número deve ser positivo!");
+            }
+        } while (num <= 0);
+        int soma = 0;
+        for (int i = 1; i <= num; i++) {
+            soma += i;
+        }
+        System.out.println("A soma total é: " + soma);
+        scan.close();
+    }
+}
---
+++ EstruturaRepeticao/src/ResolucaoExercicios/Exercicio9.java
@@ -0,0 +1,12 @@
+package ResolucaoExercicios;
+import java.util.Scanner;
+
+public class Exercicio9 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		scan.close();
+	}
+
+}
