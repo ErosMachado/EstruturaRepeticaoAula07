a3de37c0949ba2df905b4fa20b31da34b860a693
Eros Machado <112194183+ErosMachado@users.noreply.github.com>
2024-09-04 20:36:10 -0300

    Update QExercicio10.java
    Exercícios completos

--- EstruturaRepeticao/src/ResolucaoExercicios/QExercicio10.java
+++ EstruturaRepeticao/src/ResolucaoExercicios/QExercicio10.java
@@ -1,22 +1,22 @@
 package ResolucaoExercicios;
 import java.util.Scanner;
-
 public class QExercicio10 {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int memoria = 0;
-		
-		System.out.println("Digite um número inteiro positivo: ");
-		int num = scan.nextInt();
-		
-		for(int i=2; i<=num; i++) {
-			if(num%i!=0) {
-				//memoria = memoria+i;
-				System.out.println("de 2 até "+num+" os primos são: "+i);
-			}else {
-				
-			}
-			}
-		scan.close();
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um nÃºmero: ");
+        int numero = sc.nextInt();
+        for (int i = 2; i <= numero; i++) {
+            boolean primo = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+            if (primo) {
+                System.out.println(i);
+            }
+        }
+        sc.close();
+    }
+}
