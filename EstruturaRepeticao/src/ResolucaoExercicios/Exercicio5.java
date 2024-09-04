7bdadc5ce92c0574b20fa201aa0aa05961e04920
ErosMachado <erosrodriguesmachado@gmail.com>
2024-09-04 14:47:01 -0300

    exercicio 5 feito.

--- EstruturaRepeticao/src/ResolucaoExercicios/Exercicio5.java
+++ EstruturaRepeticao/src/ResolucaoExercicios/Exercicio5.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class Exercicio5 {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in); 
-
+		
+		Scanner scan = new Scanner(System.in);
+		
+		double num1=0, num2=0;
+		
+		for(int i=1;i<=12;i++) {
+		System.out.println("Informe o "+i+"º valor: ");
+		num1 = scan.nextDouble();
+		if(num1>num2) {
+			num2=num1;
+		}
+		}
+		System.out.println("O maior número digitado é o: "+num2);
 		scan.close();
 	}
-
 }
